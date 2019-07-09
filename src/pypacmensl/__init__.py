e0c46f04e268499f6aa816133f7b6ad335eb2844
voduchuy <vdhuy91@gmail.com>
2019-07-09 11:28:49 -0600

    Fix failed mpi_init on some systems

--- src/pypacmensl/__init__.py
+++ src/pypacmensl/__init__.py
@@ -1,4 +1,8 @@
 # distutils : language = c++
+
+# this must be done first because my mpi_init code is fragile on some system
+import mpi4py.MPI as MPI
+
 import pypacmensl.utils.environment as environment
 from pypacmensl.callbacks import _pacmensl_callbacks
 
