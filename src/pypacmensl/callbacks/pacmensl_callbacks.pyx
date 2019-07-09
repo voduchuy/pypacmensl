997e1810c46c56fc67e649c21c52741056960141
voduchuy <vdhuy91@gmail.com>
2019-07-09 14:27:05 -0600

    Small fix

--- src/pypacmensl/callbacks/pacmensl_callbacks.pyx
+++ src/pypacmensl/callbacks/pacmensl_callbacks.pyx
@@ -19,7 +19,7 @@ cdef int call_py_prop_obj(const int reaction, const int num_species, const int n
 cdef int call_py_t_fun_obj (double t, int num_coefs, double* outputs, void* args):
     cdef PyObject* obj = <PyObject*> args
     if args == NULL:
-        print('Null object passed to call_py_prop_obj.')
+        print('Null object passed to call_py_t_fun_obj.')
         return -1
     cdef double[::1] out_view = <double[:num_coefs]> outputs
     out_np = np.asarray(out_view)
@@ -32,7 +32,7 @@ cdef int call_py_t_fun_obj (double t, int num_coefs, double* outputs, void* args
 cdef int call_py_constr_obj(int num_species, int num_constr, int n_states, int *states, int *outputs, void *args):
     cdef PyObject* obj = <PyObject*> args
     if args == NULL:
-        print('Null object passed to call_py_prop_obj.')
+        print('Null object passed to call_py_constr_obj.')
         return -1
     cdef int[:,::1] states_view = <int[:n_states, :num_species]> states
     cdef int[:,::1] outputs_view = <int[:n_states, :num_constr]> outputs
